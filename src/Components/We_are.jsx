14316db4ab785d2f4e3d2a1fc6ccce9655a5c9e5
Srinivasan <jpsrinivasan38@gmail.com>
2023-10-22 11:42:40 +0530

    order change section 2

--- src/Components/We_are.jsx
+++ src/Components/We_are.jsx
@@ -7,8 +7,8 @@ export const We_are = () => {
   return (
     <div className={we_are.root} id='we_are'>
         <img src={planet} alt="planet image" className={we_are.bPlanet} />
-        <img src={community} alt="community image" className='w-1/2 lg:w-[30%]'/>
-        <div className='w-[80%] md:w-1/2 mx-10'>
+        <img src={community} alt="community image" className='w-1/2 lg:w-[30%] lg:order-2'/>
+        <div className='w-[80%] md:w-1/2 mx-10 lg:order-1'>
             <h1 className={common.textNabula}>Who We Are..</h1>
             <p className={we_are.p}>
             Our vibrant and dynamic software development community is a melting pot of fresh talent, eager to dive headfirst into the ever-evolving tech landscape. With a hunger for innovation and a thirst for learning, our newcomers bring a breath of fresh air to the industry. They're the rising stars, ready to shine in the constellation of coding, forging ahead with curiosity as their compass.
